7b3908f1d9fa8b83c1f9c1a40f684a697ee550a9
eneller <erikneller@protonmail.ch>
2020-09-06 17:42:49 +0200

    added check against internal xsd

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+[
++src/main/java/de/uulm/sp/pvs/util/Pair.java,5/5/5529a4ee57afe07decaaaf88f6dc8ff9a8d77247
+U
+%src/main/java/de/uulm/sp/pvs/App.java,2/e/2e2e33c92a3da5a53e66ea2ee7da507d81bea5f7
+^
+.src/main/java/de/uulm/sp/pvs/util/Sokoban.java,9/c/9ce931d577552761f2157e63c8a53bf2f51e45a6
+`
+0src/main/java/de/uulm/sp/pvs/util/lvlexample.xml,1/f/1f8ca753aff8dd8b1869f93af1b3d12995905284
+]
+-src/main/java/de/uulm/sp/pvs/util/Sokoban.xsd,4/4/4496a8bf1e15ef6612a4e2f2cae9727c857883b6
--- src/main/java/de/uulm/sp/pvs/App.java
+++ src/main/java/de/uulm/sp/pvs/App.java
@@ -1,6 +1,7 @@
 package de.uulm.sp.pvs;
 
 import de.uulm.sp.pvs.util.Sokoban;
+import de.uulm.sp.pvs.util.SokobanLevel;
 
 import java.util.Scanner;
 
@@ -10,39 +11,56 @@ import java.util.Scanner;
  */
 public class App
 {
-    public static void main( String[] args )
+    public static void main ( String[] args ) throws Exception
     {
         char[][] c = new char[7][];
-        c[0] = "#######".toCharArray();
+        c[0] = "######a".toCharArray();
         c[1] = "#.....#".toCharArray();
         c[2] = "#..$..#".toCharArray();
         c[3] = "#.$@$.#".toCharArray();
         c[4] = "#..$..#".toCharArray();
         c[5] = "#.....#".toCharArray();
         c[6] = "#######".toCharArray();
-        Sokoban s = new Sokoban(c);
+
+        SokobanLevel sl = new SokobanLevel("resources/lvlexample.xml");
+
+
+
+        Sokoban s = new Sokoban(sl.getPlayingField());
+        Sokoban t = new Sokoban(c);
+
+        runSokoban(t);
+
+
+
+    }
+
+    public static void runSokoban(Sokoban s){
         Scanner scn = new Scanner(System.in);
         boolean run=true;
-        while(run){
+        while(run) {
             System.out.println(s);
             System.out.println("Move your Player @ in one of the directions U/R/D/L");
-            if(scn.hasNext()){
+            if (scn.hasNext()) {
                 String str = scn.next();
-                if(str.equals("exit")){
-                    run=false;
+                if (str.equals("exit")) {
+                    run = false;
 
 
                 }
-                try{
-                    if(s.move(str)){
-                        System.out.println("Moved player in direction "+str);
+                try {
+                    if (s.move(str)) {
+                        System.out.println("Moved player in direction " + str);
+                    } else {
+                        System.out.println("Failed to move player in direction " + str);
                     }
-                    else{System.out.println("Failed to move player in direction "+str);}
+                } catch (IllegalStateException e) {
+                    System.out.println("Entered illegal command");
                 }
-                catch(IllegalStateException e){System.out.println("Entered illegal command");}
 
 
             }
         }
+
     }
 }
